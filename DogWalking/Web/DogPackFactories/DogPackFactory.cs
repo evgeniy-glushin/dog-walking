bf16f3e29ec529b33c6c94d67c395a752de4a00f
Eugene <glushin.evgeniy@gmail.com>
2017-10-12 23:17:59 +0300

    Domain models

---
+++ DogWalking/Web/DogPackFactories/DogPackFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Web.Models;
+
+namespace Web.DogPackFactories
+{
+    public class DogPackAbstractFactory
+    {
+        internal DogPack<Dog> CreatePacks(List<Dog> dogs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DogWalking/Web/Models/Customer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web.Models
+{
+    class DogWalkingProfessional
+    {
+        public int Id { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+        public PriceRate PriceRate { get; set; }
+        public IEnumerable<Walk> Walks { get; set; }
+        public DailySchedule GetDailySchedule { get; set; }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public IEnumerable<Dog> Dogs { get; set; }
+    }
+
+    class PriceRate
+    {
+        public DogSize DogSize { get; set; }
+        public bool IsAggressive { get; set; }
+        public decimal PricePerWalk { get; set; }
+        public byte MaxInPack { get; set; }
+        public TimeSpan WalkDuration { get; set; }
+    }
+
+    class Dog 
+    {
+        public int Id { get; set; }
+        public bool IsAggressive { get; set; }
+        public DogSize Size { get; set; }
+        public Customer Owner { get; set; }
+    }
+
+    class DogPack
+    {
+        public IEnumerable<Dog> Dogs { get; set; }
+    }
+    
+    class Schedule
+    {
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+
+    class DailySchedule
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+        public IEnumerable<TimeFrame> TimeFrames { get; set; }
+    }
+
+    class TimeFrame
+    {
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+
+    class Walk
+    {
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public bool IsDone { get; set; }
+        public DogPack DogPack { get; set; }
+    }
+
+    enum DogSize
+    {
+        None = 0,
+        Small = 1,
+        //Medium = 2, 
+        Large = 3
+    }
+
+    // InMemory store
+    // Separate libs
+    // Separate files
+    // Is there any chanse we will need to work with cats for example?
+}
