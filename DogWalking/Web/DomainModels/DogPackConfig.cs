01f0bfa9c6d0891148eca8294e5a2f017a23b593
Eugene <glushin.evgeniy@gmail.com>
2017-10-23 22:21:15 +0300

    Remove unused class.

--- DogWalking/Web/DomainModels/DogPackConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Models
-{
-    public class DogPackConfig
-    {
-        public DogSize DogSize { get; set; }
-        public bool IsAggressive { get; set; }
-        public byte MaxPackSize { get; set; }
-    }
-}
